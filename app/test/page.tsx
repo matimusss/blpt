5505934cdee0da7f42bc686c40f9984a4d7b4075
unknown <ejemplo@email.com>
2024-05-06 20:03:09 -0300

    ASD

---
+++ app/test/page.tsx
@@ -0,0 +1,22 @@
+"use client"
+//import Image from "next/image";
+import { ContactUs} from '../../components/ContactUs/ContactUs';
+import {CarouselCard} from '../../components/CarouselCard/CarouselCard';
+import {HeaderTabs} from '../../lib/HeaderTabs/HeaderTabs';
+import {FooterLinks} from '../../lib/FooterLinks/FooterLinks';
+import {TableSort} from '../../lib/TableSort/TableSort';
+import Link from 'next/link';
+
+
+export default function Home() {
+  return (
+   <div>
+<div><HeaderTabs /></div>          
+<div><FooterLinks /></div>
+  </div>
+
+<Button component={Link} href="/">
+Next link button
+</Button>
+  );
+}
