1ae347675c0001e104b711e0e7c08c778198706d
matimusa@gmail.com <ejemplo@email.com>
2024-05-07 05:19:34 -0300

    ASD

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 "use client"
 //import Image from "next/image";
 import { ContactUs} from '../components/ContactUs/ContactUs';
-import {TableSort} from '../lib/TableSort/TableSort';
 import Link from "next/link";
 import { Button } from '@mantine/core';
 
@@ -11,7 +10,6 @@ export default function Home() {
 <div><ContactUs /></div>          
 
 <Button component={Link} href="/test">  Next link button  </Button>
-<div><TableSort /></div> 
 
   </div>
 
