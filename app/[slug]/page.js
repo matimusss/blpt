830ea7144118c465a98201a52b3db9ecaddb7057
matimusa@gmail.com <ejemplo@email.com>
2024-05-24 16:19:04 -0300

    asd

--- app/[slug]/page.js
+++ app/[slug]/page.js
@@ -1,23 +1,37 @@
-async function fetching() {
-  const response = await fetch('https://sonicjs-cf2.pages.dev/v1/pages').then((res) => res.json());
-  return response.data.map((post) => ({
-    slug: post.slug,
-    name: post.name,
-  }));
-  
-  const respuesta = await fetching();
-  const allSlugs = respuesta.map((rta) => rta.slug);
-  const allNames = respuesta.map((rta) => rta.name);
-}
+
 
 export async function generateStaticParams() {
+  async function fetching() {
+    const response = await fetch('https://sonicjs-cf2.pages.dev/v1/pages').then((res) => res.json());
+    return response.data.map((post) => ({
+      slug: post.slug,
+      name: post.name,
+    }));}
+    const respuesta = await fetching();
+    const allSlugs = respuesta.map((rta) => rta.slug);
   return allSlugs;
 }
 
-export default function Page({ params }) {
-  return (
+export default async function Page({ params }) {
+  const slugs = {params}
+
+  async function fetching2() {
+    
+    const response2 = await  fetch(`https://sonicjs-cf2.pages.dev/v1/pages?filters[slug][$eq]=${params}`).then((res) => res.json());
+    return response2.data.map((post2) => ({
+      slug: post2.slug,
+      name: post2.name,
+      css: post2.css_code,
+      html: post2.html_code,
+    }));}
+    
+    const respuesta2 = await fetching2();
+    return (
     <div>           
-  
+   {response2.slug}
+   {response2.name}
+   {response2.html}
+   {response2.css}
     </div>
   );
 }
