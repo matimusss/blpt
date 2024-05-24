9d3cb12108ca3a644e443300c10bfb5610c2ac47
matimusa@gmail.com <ejemplo@email.com>
2024-05-24 08:35:46 -0300

    asd

--- app/[slug]/page.js
+++ app/[slug]/page.js
@@ -2,14 +2,7 @@ export async function generateStaticParams() {
   const response = await fetch('https://sonicjs-cf2.pages.dev/v1/pages').then((res) => res.json());
 
   return response.data.map((post) => ({
-      id: post.ID.toString(),
-      name: post.name,
       slug: post.slug,
-      htmlCode: post.html_code,
-      cssCode: post.css_code,
-      createdOn: new Date(post.createdOn),
-      updatedOn: new Date(post.updatedOn),
-      total: post.total
   }));
 }
 
@@ -18,12 +11,10 @@ export async function generateStaticParams() {
 
   export default function Page({ params }) {
     const { slug } = params.slug;
-    const { name } = params.name;
-    
     return (
       <div>           
         probando generateStaticParams...
-        NAME es : {name}
+        NAME es : {slug}
       
       </div>
     );
