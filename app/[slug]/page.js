2241018b0256319d6a6eae22f6f18ab67ba41918
matimusa@gmail.com <ejemplo@email.com>
2024-05-24 16:22:27 -0300

    asd

--- app/[slug]/page.js
+++ app/[slug]/page.js
@@ -1,22 +1,24 @@
-
-
 export async function generateStaticParams() {
-  async function fetching() {
-    const response = await fetch('https://sonicjs-cf2.pages.dev/v1/pages').then((res) => res.json());
-    return response.data.map((post) => ({
+ 
+    const posts = await fetch('https://sonicjs-cf2.pages.dev/v1/pages').then((res) => res.json())
+ 
+    return posts.data.map((post) => ({
       slug: post.slug,
-      name: post.name,
-    }));}
-    const respuesta = await fetching();
-    const allSlugs = respuesta.map((rta) => rta.slug);
-  return allSlugs;
+    }))
 }
+
+
+
+
+
+
+
+
+
 
 export default async function Page({ params }) {
   const slugs = {params}
-
   async function fetching2() {
-    
     const response2 = await  fetch(`https://sonicjs-cf2.pages.dev/v1/pages?filters[slug][$eq]=${params}`).then((res) => res.json());
     return response2.data.map((post2) => ({
       slug: post2.slug,
@@ -24,7 +26,6 @@ export default async function Page({ params }) {
       css: post2.css_code,
       html: post2.html_code,
     }));}
-    
     const respuesta2 = await fetching2();
     return (
     <div>           
