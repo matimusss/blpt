c950586547cc162bb7ebf4d915d297683d440268
matimusa@gmail.com <ejemplo@email.com>
2024-05-24 18:36:39 -0300

    asd

--- app/[slug]/page.js
+++ app/[slug]/page.js
@@ -21,8 +21,7 @@ export default async function Page({ params }) {
   
     const response2 = await fetch(`https://sonicjs-cf2.pages.dev/v1/pages?filters[slug][$eq]=${slugs}`).then((res) => res.json());
     const postData = response2.data; // Como la respuesta solo tiene un objeto, no necesitas mapear
-
-    const respuesta2 = await fetching2();
+  
     return (
      <div>           
    {postData.name}
