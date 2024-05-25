19a2c4110a4c42d6d642e14831b887c5e6d6e004
matimusa@gmail.com <ejemplo@email.com>
2024-05-24 21:39:03 -0300

    asd

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
     <head>
+    <script src="https://cdn.tailwindcss.com"></script>
       <ColorSchemeScript />
     </head>
     <body >
