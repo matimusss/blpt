3d24b8cbbd0d9951305577e52604d51686b197be
matimusa@gmail.com <ejemplo@email.com>
2024-05-25 23:54:48 -0300

    ASD

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import Script from 'next/script'
 export default  async function Layout({ children }) {    
 
  
-  const headerRes = await fetch(`https://sonicjs-cf2.pages.dev/v1/assets?filters[name][$eq]=header}`);
+  const headerRes = await fetch(`https://sonicjs-cf2.pages.dev/v1/assets?filters[name][$eq]=header`);
   
   const footerRes = await fetch(`https://sonicjs-cf2.pages.dev/v1/assets?filters[name][$eq]=footer`);
   
