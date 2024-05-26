dbccbdb244b292d27bd934fc27384b0df70e152f
matimusa@gmail.com <ejemplo@email.com>
2024-05-25 23:23:26 -0300

    ASD

--- app/[slug]/page.js
+++ app/[slug]/page.js
@@ -1,23 +1,27 @@
  export async function generateStaticParams() {
     const posts = await fetch('https://sonicjs-cf2.pages.dev/v1/pages').then((res) => res.json())
+
     return posts.data.map((post) => ({
       slug: post.slug,
     }))
-}
+
+
+
+  }
 export default async function Page({ params }) {
   const slug = params;
-  const pageRes = await fetch(`https://sonicjs-cf2.pages.dev/v1/pages?filters[slug][$eq]=${slug.slug}`).then((res) =>    res.json(),
-  {
-    next: { revalidate: 0 } 
-  });
-  const pageData = await pageRes.json();
+  //const pageRes = await fetch(`https://sonicjs-cf2.pages.dev/v1/pages?filters[slug][$eq]=${slug.slug}`).then((res) =>    res.json(),
+  //{
+  //  next: { revalidate: 0 } 
+  //});
+  //const pageData = await pageRes.json();
 
 // <style dangerouslySetInnerHTML={{ __html: response2.data[0].css_code }} />
 // {response2.data[0].html_code} ese funca
-
+//<div dangerouslySetInnerHTML={{ __html: pageData.data[0].html_code }} />
   return (
      <div>       
- <div dangerouslySetInnerHTML={{ __html: pageData.data[0].html_code }} />
+
     </div>
   );
 }
