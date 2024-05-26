ecaadfc9916a875a8a60787df6d5e1f66b352f0a
matimusa@gmail.com <ejemplo@email.com>
2024-05-25 22:27:53 -0300

    ASD

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,20 @@
+export const dynamic = 'force-dynamic';
 import Script from 'next/script'
 
 export default  async function Layout({ children }) {
     const timestamp = new Date().getTime();
-    const headerRes = await fetch(`https://sonicjs-cf2.pages.dev/v1/assets?filters[name][$eq]=header&timestamp=${timestamp}`);
-    const footerRes = await fetch(`https://sonicjs-cf2.pages.dev/v1/assets?filters[name][$eq]=footer&timestamp=${timestamp}`);
+    const headerRes = await fetch(`https://sonicjs-cf2.pages.dev/v1/assets?filters[name][$eq]=header&timestamp=${timestamp}`,
+    {
+			next: { revalidate: 0 } 
+    })
+    ;
+
+    const footerRes = await fetch(`https://sonicjs-cf2.pages.dev/v1/assets?filters[name][$eq]=footer&timestamp=${timestamp}`,
+    {
+			next: { revalidate: 0 } 
+		}) ;
+
+
      const headerData = await headerRes.json();
      const footerData = await footerRes.json();
 
