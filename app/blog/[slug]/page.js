62f2ba457d404a302b8b42b6f357da3599476645
matimusa@gmail.com <ejemplo@email.com>
2024-05-26 02:02:26 -0300

    ASD

--- app/blog/[slug]/page.js
+++ app/blog/[slug]/page.js
@@ -1,12 +1,20 @@
 
 export async function generateStaticParams() {
   var Slugify = require('slugifyjs').fromLocale('en');
+
+
+
   const posts = await fetch('https://sonicjs-cf2.pages.dev/v1/posts').then((res) => res.json())
-    return posts.data.map((post) => ({
-      title: post.title, 
+    return posts.data.map((post) => ({ 
       slug: Slugify.parse(post.title),
     }))
 }
+
+
+
+
+
+
 export default async function Page({ params }) {
   const slug = params.slug;
 const title = params.title
