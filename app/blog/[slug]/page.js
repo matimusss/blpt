b25fdf15cb5001811b07eaba400898e6c44e0350
matimusa@gmail.com <ejemplo@email.com>
2024-05-26 02:05:01 -0300

    ASD

--- app/blog/[slug]/page.js
+++ app/blog/[slug]/page.js
@@ -2,8 +2,6 @@
 export async function generateStaticParams() {
   var Slugify = require('slugifyjs').fromLocale('en');
 
-
-
   const posts = await fetch('https://sonicjs-cf2.pages.dev/v1/posts').then((res) => res.json())
     return posts.data.map((post) => ({ 
       slug: Slugify.parse(post.title),
@@ -17,16 +15,15 @@ export async function generateStaticParams() {
 
 export default async function Page({ params }) {
   const slug = params.slug;
-const title = params.title
-  const blogData = await 
-  fetch(`https://sonicjs-cf2.pages.dev/v1/posts?filters[title][$eq]=${title}`).then((res) =>    res.json());
- 
+ // const blogData = await 
+///  fetch(`https://sonicjs-cf2.pages.dev/v1/posts?filters[title][$eq]=${title}`).then((res) =>    res.json());
+ //  <div dangerouslySetInnerHTML={{ __html: blogData.data[0].title }} />
+//  <div dangerouslySetInnerHTML={{ __html: blogData.data[0].body }} />
 
 
   return (
      <div>       
-  <div dangerouslySetInnerHTML={{ __html: blogData.data[0].title }} />
-  <div dangerouslySetInnerHTML={{ __html: blogData.data[0].body }} />
+
     </div>
   );
 }
