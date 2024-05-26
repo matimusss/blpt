56882c6daaf3688de49e5303dcb0eb11f33648f0
matimusa@gmail.com <ejemplo@email.com>
2024-05-26 00:05:14 -0300

    ASD

--- app/[slug]/page.js
+++ app/[slug]/page.js
@@ -9,16 +9,16 @@
   }
 export default async function Page({ params }) {
   const slug = params;
-//  const pageRes = await fetch(`https://sonicjs-cf2.pages.dev/v1/pages?filters[slug][$eq]=${slug.slug}`).then((res) =>    res.json());
+  const pageRes = await fetch(`https://sonicjs-cf2.pages.dev/v1/pages?filters[slug][$eq]=${slug.slug}`).then((res) =>    res.json());
   //const pageData = await pageRes.json();
 
 // <style dangerouslySetInnerHTML={{ __html: response2.data[0].css_code }} />
-// <div dangerouslySetInnerHTML={{ __html: pageData.data[0].html_code }} />
+
 // {response2.data[0].html_code} ese funca
 
   return (
      <div>       
-
+// <div dangerouslySetInnerHTML={{ __html: pageRes.data[0].html_code }} />
     </div>
   );
 }
