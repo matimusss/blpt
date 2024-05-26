fc9c28af7842eb30e9512a629d4a189ff2ed824c
matimusa@gmail.com <ejemplo@email.com>
2024-05-26 01:11:39 -0300

    ASD

--- app/blog/[slug]/page.js
+++ app/blog/[slug]/page.js
@@ -3,23 +3,22 @@ export async function generateStaticParams() {
   var Slugify = require('slugifyjs').fromLocale('en');
   const posts = await fetch('https://sonicjs-cf2.pages.dev/v1/posts').then((res) => res.json())
     return posts.data.map((post) => ({
+      title: post.title
       slug: Slugify.parse(post.title),
     }))
 }
 export default async function Page({ params }) {
-  const slug = params;
+  const slug = params.slug;
+const title = params.title
+  const blogData = await 
+  fetch(`https://sonicjs-cf2.pages.dev/v1/posts?filters[title][$eq]=${title}`).then((res) =>    res.json());
+ 
 
-  //const response2 = await fetch(`https://sonicjs-cf2.pages.dev/v1/posts?filters[title][$eq]=${slug.slug}`).then((res) =>    res.json());
-  //const blogData = await response2.json();
-  // {response2.data[0].html_code} ese funca
-
-
-  //<div dangerouslySetInnerHTML={{ __html: blogData.data[0].title }} />
-  //<div dangerouslySetInnerHTML={{ __html: blogData.data[0].body }} />
 
   return (
      <div>       
-TODO
+  <div dangerouslySetInnerHTML={{ __html: blogData.data[0].title }} />
+  <div dangerouslySetInnerHTML={{ __html: blogData.data[0].body }} />
     </div>
   );
 }
