58e79f6bffed66e2272ea2621891bc66d3d0b26d
matimusa@gmail.com <ejemplo@email.com>
2024-07-09 06:32:40 -0300

    asd

--- app/producto/[slug]/page.js
+++ app/producto/[slug]/page.js
@@ -3,7 +3,8 @@
 export async function generateStaticParams() {
  // var Slugify = require('slugifyjs').fromLocale('en');
   //slug: Slugify.parse(post.title),
-  const posts = await fetch('https://sonicjs-cf2.pages.dev/v1/product_full_details').then((res) => res.json())
+  
+  const posts = await fetch('https://sonicjs-cf2.pages.dev/v1/product-full-details').then((res) => res.json())
     return posts.data.map((post) => ({ 
       slug: post.slug,
     }))
@@ -16,7 +17,7 @@ export default async function Page({ params }) {
   const slug = params;
  // const blogData = await 
 // const blogData = await fetch(`https://sonicjs-cf2.pages.dev/v1/nc/product_full_details?filters[slug][$eq]=${slug.slug}`).then((res) =>    res.json());
- const blogData = await fetch(`https://sonicjs-cf2.pages.dev/v1/product_full_details`).then((res) =>    res.json());
+ const blogData = await fetch(`https://sonicjs-cf2.pages.dev/v1/product-full-details`).then((res) =>    res.json());
 
   return (
      <div>       
