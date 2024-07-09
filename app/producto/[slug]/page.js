ad4b0c259cebcc973f794a9a6f02d2b47ef1801e
matimusa@gmail.com <ejemplo@email.com>
2024-07-09 06:29:28 -0300

    asd

--- app/producto/[slug]/page.js
+++ app/producto/[slug]/page.js
@@ -16,7 +16,7 @@ export default async function Page({ params }) {
   const slug = params;
  // const blogData = await 
 // const blogData = await fetch(`https://sonicjs-cf2.pages.dev/v1/nc/product_full_details?filters[slug][$eq]=${slug.slug}`).then((res) =>    res.json());
- const blogData = await fetch(`https://sonicjs-cf2.pages.dev/v1/nc/product_full_details`).then((res) =>    res.json());
+ const blogData = await fetch(`https://sonicjs-cf2.pages.dev/v1/product_full_details`).then((res) =>    res.json());
 
   return (
      <div>       
