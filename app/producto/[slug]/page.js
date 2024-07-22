7c9ee635eb88b952bd926064e9afa77ee1808e47
matimusa@gmail.com <ejemplo@email.com>
2024-07-22 12:01:07 -0300

    asd

--- app/producto/[slug]/page.js
+++ app/producto/[slug]/page.js
@@ -2,8 +2,6 @@ import React from 'react';
 
 // Función para obtener los parámetros estáticos
 export async function generateStaticParams() {
-
-
   const posts = await fetch('https://sonicjs-cf2.pages.dev/v1/product-min-details').then((res) => res.json());
   return posts.map((post) => ({ 
     slug: post.slug,
@@ -13,7 +11,7 @@ export async function generateStaticParams() {
 // Función para reemplazar los marcadores de posición en el HTML
 function replacePlaceholders(htmlString, data) {
   return htmlString.replace(/{{(.*?)}}/g, (_, key) => {
-    return data[key.trim()] || '';
+    return data[key.trim()] || ''; // Reemplaza los marcadores de posición con los valores del objeto de datos
   });
 }
 
@@ -22,31 +20,14 @@ export default async function Page({ params }) {
   const slug = params.slug;
   const blogData = await fetch(`https://sonicjs-cf2.pages.dev/v1/getProductBySlug/${slug}`).then((res) => res.json());
 
-  
-  
-  
+  // Fetch additional data if needed
   const productRes = await fetch(`https://sonicjs-cf2.pages.dev/v1/nc/assets?filters[name][$eq]=product`);
-  const productData = await productRes.json();  
-  console.log(productData);
+  const productData = await productRes.json();
   
+  // Extraer el código HTML desde la respuesta JSON
+  const htmlTemplate = productData.data[0].html_code;
   
-  
-  
-  
-  // HTML de ejemplo con marcadores de posición
-  const htmlTemplate = `
-    <div>
-    DESDE HTMLTEMPLATE
-      <h1>{{product_name}}</h1>
-      <p><strong>Descripción:</strong> {{product_description}}</p>
-      <p><strong>SKU:</strong> {{sku}}</p>
-      <p><strong>Precio de Venta:</strong> {{sale_price}}</p>
-      <p><strong>Precio Comparativo:</strong> {{compare_price}}</p>
-      <p><strong>Precio de Compra:</strong> {{buying_price}}</p>
-      <p><strong>Cantidad:</strong> {{quantity}}</p>
-      <p><strong>Tipo de Producto:</strong> {{product_type}}</p>
-    </div>
-  `;
+  console.log(htmlTemplate); // Verificar el contenido del HTML
 
   // Reemplaza los marcadores de posición con los valores del producto
   const htmlContent = replacePlaceholders(htmlTemplate, blogData);
