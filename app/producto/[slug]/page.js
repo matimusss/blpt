eac3e36426a3c8cc2cea8037b4c6d2864c9aabea
matimusa@gmail.com <ejemplo@email.com>
2024-07-22 21:41:19 -0300

    asd

--- app/producto/[slug]/page.js
+++ app/producto/[slug]/page.js
@@ -28,19 +28,6 @@ function replacePlaceholders(htmlString, data) {
   });
 }
 
-export function EmblaCarousel() {
-  const [emblaRef] = useEmblaCarousel()
-
-  return (
-    <div className="embla" ref={emblaRef}>
-      <div className="embla__container">
-        <div className="embla__slide">Slide 1</div>
-        <div className="embla__slide">Slide 2</div>
-        <div className="embla__slide">Slide 3</div>
-      </div>
-    </div>  
-  )
-}
 
 
 
